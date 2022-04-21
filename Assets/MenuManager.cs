bcc2ce10f0b9c5b8cbfee3ff906db476f392e835
PedroLazari223 <pedrolazarimachado@gmail.com>
2022-04-20 22:44:40 -0300

    Adding Credential pages

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject regPage1, regPage2, regPage3;
+
+    [SerializeField]
+    InputField emailCredential, passwordCredential;
+
+    [SerializeField]
+    InputField emailForPasswordReset;
+
+    Hashtable cred = new Hashtable();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cred.Add("pedro@gmail.com", "123");
+
+        regPage1.SetActive(true);
+        regPage2.SetActive(false);
+        regPage3.SetActive(false);
+    }
+
+    public void enter()
+    {
+        if (cred.ContainsKey(emailCredential.text))
+        {
+            if ((string)cred[emailCredential.text] == (string)passwordCredential.text) {
+                regPage1.SetActive(false);
+                regPage2.SetActive(false);
+                regPage3.SetActive(false);
+            }
+        }
+        
+    }
+
+    public void forgotMyPassword()
+    {
+        regPage1.SetActive(false);
+        regPage2.SetActive(true);
+        regPage3.SetActive(false);
+    }
+
+    public void sendPassword()
+    {
+        if(emailForPasswordReset.text != "")
+        {
+            regPage1.SetActive(false);
+            regPage2.SetActive(false);
+            regPage3.SetActive(true);
+        }        
+    }
+
+    public void tryEnterAgain()
+    {
+        regPage1.SetActive(true);
+        regPage2.SetActive(false);
+        regPage3.SetActive(false);
+    }
+}
